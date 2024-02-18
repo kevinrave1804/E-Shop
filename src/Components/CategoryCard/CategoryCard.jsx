85bb0e8f234c24ae847bc3da7c9aae258de2ca28
Kevin Ramirez <kevinrave1804@gmail.com>
2024-02-17 21:25:11 -0500

    Categories Section base

---
+++ src/Components/CategoryCard/CategoryCard.jsx
@@ -0,0 +1,53 @@
+import React, { useEffect, useState } from 'react'
+import ProductCard from '../ProductCard/ProductCard'
+import { endPoints } from '../../Services/endPoints'
+
+function CategoryCard({ category }) {
+    const [productsCategory, setProductsCategory] = useState([])
+
+    async function getProductsCategory(category) {
+        const response = await fetch(endPoints.products.getProductsCategory(category))
+        const data = await response.json()
+        setProductsCategory(data)
+    }
+
+    useEffect(() => {
+        try {
+            getProductsCategory(category)
+        } catch (error) {
+            console.log(error);
+        }
+    }, [])
+
+    return (
+        <section>
+            <div className="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8">
+                <div className="grid grid-cols-1 gap-4 lg:grid-cols-3 lg:items-stretch">
+                    <div className="grid place-content-center rounded bg-gray-100 p-6 sm:p-8">
+                        <div className="mx-auto max-w-md text-center lg:text-left">
+                            <header>
+                                <h2 className="text-xl font-bold text-gray-900 sm:text-3xl">{category.toUpperCase()}</h2>
+
+                                <p className="mt-4 text-gray-500">
+                                    Explore our diverse range of categories, each curated to cater to your unique needs and interests. Discover quality products in every niche, making your online shopping experience a breeze. From fashion and electronics to home decor and beyond, find the perfect item that suits your style and preferences. Happy shopping!
+                                </p>
+                            </header>
+                        </div>
+                    </div>
+
+                    <div className="lg:col-span-2 lg:py-8">
+                        <ul className="grid grid-cols-2 gap-4">
+                            {productsCategory?.map(product => (
+                                <li>
+                                    <ProductCard key={product.id} product={product} />
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default CategoryCard
--- src/Pages/Categories/Categories.jsx
+++ src/Pages/Categories/Categories.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Layout from '../../Components/Layout/Layout'
+import { endPoints } from '../../Services/endPoints'
+import CategoryCard from '../../Components/CategoryCard/CategoryCard'
 
 function Categories() {
+    const [categories, setCategories] = useState(null)
+
+    async function fecthCategories() {
+        const response = await fetch(endPoints.products.getAllCategories)
+        const data = await response.json()
+        setCategories(data)
+    }
+
+    useEffect(() => {
+        try {
+            fecthCategories()
+        } catch (error) {
+            console.log(error);
+        }
+    }, [])
+
     return (
-        <Layout>Categories</Layout>
+        <Layout>
+            <section className='text-center'>
+                <h2 className='text-2xl font-bold md:text-5xl'>Categories</h2>
+                <div className='my-6'>
+                    {categories?.map((category, index) => (
+                        <CategoryCard key={index} category={category} />
+                    ))}
+                </div>
+            </section>
+        </Layout>
     )
 }
 
